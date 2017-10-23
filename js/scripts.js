51f635309b5dce411b01e53f941e869ae2badc27
Cheryl Frankenfield and Lena Ha <cheryl@>
2017-10-23 16:53:01 -0700

    initialize git, make pseudocode

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+var suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+var cards = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
+var deck = [];
+
+
+
+  //"<li>" + card + "of" + suit + "</li>"
+
+});
